05df7f46adcc9c6de4bfaedddb58cbe514687c48
Sarun Daunghirun <sdsarun@outlook.com>
2023-04-17 10:30:02 +0700

    add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div>Left box</div>
+            <div>Center box</div>
+            <div>Right box</div>
+        </nav>
+    );
+}
+
+export default Navbar;
