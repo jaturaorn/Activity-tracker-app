f8586fb54720113ebceca348bbc47e04eec624a4
= <sdsarun@outlook.com>
2023-04-24 20:11:57 +0700

    complete footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Main from '../components/Main'
 import Quote from "../components/Quote"
+import Footer from '../components/Footer';
 
 function Home() {
   return (
-    
-    <Main/>
-    
+
+    <>
+      <Main />
+      <Footer />
+    </>
   );
 }
 
