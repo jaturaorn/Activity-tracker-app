1c517da2f0591f47232ee9e6205ad9483e09ba9e
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-04-24 20:31:10 +0700

    imagesslider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Main from '../components/Main'
 import Quote from "../components/Quote"
+import Imageslider from "../components/Imageslider" 
 
 function Home() {
   return (
-    
+    <div> 
     <Main/>
-    
+    <Imageslider/>
+    </div>
   );
 }
 
