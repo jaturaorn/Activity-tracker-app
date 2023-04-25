70242a28003e623c3559d952a268a5e9b400cfe9
Sarun Daunghirun <sdsarun@outlook.com>
2023-04-25 18:43:03 +0700

    asdfasdf

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Main from '../components/Main'
 import Quote from "../components/Quote"
+import Navbar from '../components/Navbar';
 
 function Home() {
   return (
-    
     <Main/>
-    
+
   );
 }
 
