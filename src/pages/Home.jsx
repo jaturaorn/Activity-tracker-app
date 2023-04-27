c823bef9011d909c456735bae4dee43265562557
Sireethon Bunchongpru <werrty65@gmail.com>
2023-04-27 16:59:16 +0700

    Update Recipe page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Main from '../components/Main'
 import Quote from "../components/Quote"
 import Imageslider from "../components/Imageslider"
+import Footer from "../components/Footer"
 
 function Home() {
   return (
     <>
+        <Imageslider/>
       <Main/>
+      <Footer/>
+
     </>
   );
 }
