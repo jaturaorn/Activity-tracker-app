b1e550233a86df9df0c1e6e4e51ff63488dfd415
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-05-10 20:34:18 +0700

    Test card

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,16 +1,39 @@
+import pic1 from '../image/cardworkout1.jpg';
+import pic2 from '../image/cardworkout2.jpg';
+import pic3 from '../image/cardworkout3.jpg';
+import pic4 from '../image/cardworkout4.jpg';
 
-const urlImage = 'https://unsplash.com/photos/lrQPTQs7nQQ';
 
 const ImageCard = () => {
-  
+  const mockdata = [
+    {
+      img: pic1,
+      title: 'Card Workout',
+    },
+    {
+      img: pic2,
+      title: 'Card Workout',
+    },
+    {
+      img: pic3,
+      title: 'Card Workout',
+    },
+    {
+      img: pic4,
+      title: 'Card Workout',
+    }
+  ]
   return (
-    <div className="grid grid-cols-1 bg-main-purple rounded-md">
-      <h2 className="font-bold my-2">HIIT</h2>
-       <img src={urlImage} className="rounded-md rounded-b-none"/>
-       <div className="my-2 p-4">
-        <h3 className="font-semibold">10 MIN WITH HI-INTENSE</h3>
-       </div>
-    </div>
+    <main className="container mx-auto py-36 px-8">
+      <div className="grid lg:grid-cols-4 gap-6">
+        {mockdata.map(card =>(
+          <div className='shadow-lg rounded-lg'>
+            <img className='rounded-t-lg' src={card.img} alt=''/>
+            <h3 className='text-3xl font-bold text-slate-700 mb-3 text-center'>{card.title}</h3>
+          </div>
+        ))}
+      </div>
+    </main>
   )
 }
 
