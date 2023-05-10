1f626b8174c70a38d167dc7edf812b0178c29807
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-05-10 22:24:06 +0700

    almost done

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -8,28 +8,30 @@ const ImageCard = () => {
   const mockdata = [
     {
       img: pic1,
-      title: 'Card Workout',
+      title: 'Yoga 15 Min',
     },
     {
       img: pic2,
-      title: 'Card Workout',
+      title: 'Weight Training',
     },
     {
       img: pic3,
-      title: 'Card Workout',
+      title: 'Abs workout',
     },
     {
       img: pic4,
-      title: 'Card Workout',
+      title: 'aerobics Workout',
     }
   ]
   return (
-    <main className="container mx-auto py-36 px-8">
+    <main className="container mx-auto py-20 px-8">
+      <h2 className='text-3xl font-bold'>HIIT</h2>
+      <p className='text-large font-semibold my-3'>Ready to get heart pumping?</p>
       <div className="grid lg:grid-cols-4 gap-6">
         {mockdata.map(card =>(
-          <div className='shadow-lg rounded-lg'>
+          <div className='shadow-lg rounded-lg bg-main-purple'>
             <img className='rounded-t-lg' src={card.img} alt=''/>
-            <h3 className='text-3xl font-bold text-slate-700 mb-3 text-center'>{card.title}</h3>
+            <h3 className='text-xl text-white mt-2 mb-2 text-center'>{card.title}</h3>
           </div>
         ))}
       </div>
