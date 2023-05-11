bd3c9190d8f3fab0abe2b85d55b9841f40bc3d87
Sarun Daunghirun <sdsarun@outlook.com>
2023-05-11 21:23:14 +0700

    not done yet!, need seperate and more...

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,13 +3,14 @@ import ActivityTracker from '../components/ActivityTracker'
 import GoalTracker from '../components/GoalTracker'
 
 function Dashboard() {
-  const [test, setText] = useState(0);
 
   return (
     <div className="flex flex-col items-center h-full">
       {/* Status Section */}
       <div className="w-full">
         <div className="container border-2 border-yellow-500 mx-auto">
+
+          {/* code here */}
           <h1>Status section</h1>
           <h1>Status section</h1>
           <h1>Status section</h1>
@@ -22,6 +23,8 @@ function Dashboard() {
           <h1>Status section</h1>
           <h1>Status section</h1>
           <h1>Status section</h1>
+          <h1 className="text-3xl">Code here.................</h1>
+
         </div>
       </div>
 
@@ -29,6 +32,9 @@ function Dashboard() {
       {/* Graph Section */}
       <div className="w-full">
         <div className="container border-2 border-red-500 mx-auto">
+
+          {/* code here */}
+
           <h1>Graph section</h1>
           <h1>Graph section</h1>
           <h1>Graph section</h1>
@@ -37,16 +43,18 @@ function Dashboard() {
           <h1>Graph section</h1>
           <h1>Graph section</h1>
           <h1>Graph section</h1>
+          <h1 className="text-3xl">Code here.................</h1>
+
         </div>
       </div>
 
 
-      {/* Bottom Section */}
-      <div className="w-full flex-grow h-screen-2/4">
-        <div className="container mx-auto border-2 border-yellow-700">
-          <h1 className="text-3xl font-bold py-2">Your Tracking</h1>
+      {/* Activity and Goal Section */}
+      <div className="w-full flex-grow pb-14">
+        <div className="container mx-auto ">
+          <h1 className="text-3xl font-bold py-2 tracking-wide">Your Tracking</h1>
         </div>
-        <div className="container mx-auto grid grid-cols-4 gap-x-4 max-md:grid-cols-1">
+        <div className="container mx-auto grid grid-cols-12 gap-x-8 max-md:grid-cols-1 max-lg:grid-cols-1 max-xl:gap-x-0 max-xl:gap-y-10">
           <ActivityTracker />
           <GoalTracker />
         </div>
